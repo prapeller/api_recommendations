e9768c52fd277a1935a41b8c944ef2d96afea175
prapeller <prapeller@mail.ru>
2023-11-09 09:47:53 +0300

    chore: etl load by chunks

--- etl/etl_postgres_to_vector_recommendations/helpers/load.py
+++ etl/etl_postgres_to_vector_recommendations/helpers/load.py
@@ -1,9 +1,18 @@
 import numpy as np
 import requests
 from backoff import constant, on_exception
+from logger_config import logger
 
 from core.config import settings
-from logger_config import logger
+
+
+def chunks_generator(lst, chunk_size):
+    """Yield chunk_sized elements chunks from lst sequence."""
+    for i in range(0, len(lst), chunk_size):
+        yield lst[i:i + chunk_size]
+
+
+VECTORS_CHUNK_SIZE = 100
 
 
 class VectorLoader:
@@ -16,9 +25,11 @@ class VectorLoader:
         vectors = {k: v.tolist() for k, v in vectors.items()}
         headers = {'Authorization': settings.SERVICE_TO_SERVICE_SECRET,
                    'Service-Name': 'etl_from_postgres_to_vector'}
-        try:
-            response = requests.post(self.load_url, json=vectors, headers=headers)
-            response.raise_for_status()
-            logger.debug("Vectors successfully sent to the Vector service.")
-        except requests.exceptions.RequestException as e:
-            logger.error(f"Failed to send vectors to the Vector service. Error: {str(e)}")
+        for vectors_chunk in chunks_generator(vectors, VECTORS_CHUNK_SIZE):
+            try:
+                response = requests.post(self.load_url, json=vectors_chunk, headers=headers)
+                response.raise_for_status()
+                logger.debug("Vectors successfully sent to the Vector service.")
+            except requests.exceptions.RequestException as e:
+                logger.error(f"Failed to send vectors to the Vector service. Error: {str(e)}")
+                raise
